eb7836cea98256fd2ea5cba4b1a4365dbace3d20
Andrii Khudolii <khudolii.a.s@gmail.com>
2022-07-11 17:13:41 +0200

    feat: added base light on\off wand with speech recognition; added grabbing option; added base awada kedavra wand.

--- Assets/Scripts/WandController.cs
+++ Assets/Scripts/WandController.cs
@@ -5,25 +5,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
-public class SpeechRecognition
+public class WandController : MonoBehaviour
 {
+
+    public GameObject pointerLight;
+    public GameObject endWandMaterial;
+
     private KeywordRecognizer keywordRecognizer;
-    private Dictionary<string, Action>  actions = new Dictionary<string, Action>();
-    
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+  
     // Start is called before the first frame update
     void Start()
     {
-        actions.Add("forward", OnAction);        
-        actions.Add("left", OnAction);        
-        actions.Add("right", OnAction);        
-        actions.Add("back", OnAction);
-        actions.Add("pick up", OnAction);
-        actions.Add("put down", OnAction);
-        
+        actions.Add("light", changeWandStatus);
+
         keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += OnRecognizedSpeech;
         keywordRecognizer.Start();
+
     }
+
 
     private void OnRecognizedSpeech(PhraseRecognizedEventArgs speech)
     {
@@ -31,9 +32,11 @@ public class SpeechRecognition
         actions[speech.text].Invoke();
     }
 
-    private void OnAction()
+    private void changeWandStatus()
     {
-        
-    }
+        Debug.Log("i'm here");
 
+        pointerLight.SetActive(!pointerLight.activeSelf);
+        endWandMaterial.SetActive(!endWandMaterial.activeSelf);
+    }
 }
