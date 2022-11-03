849008b496e981ab06a9f56a2166caf1e3a7d2bd
Andrii Khudolii <khudolii.a.s@gmail.com>
2022-11-03 11:09:45 +0100

    feat: update outline script to remove materials when script is disabled

--- Assets/Scripts/outline/Outline.cs
+++ Assets/Scripts/outline/Outline.cs
@@ -1,135 +1,102 @@
-﻿//
-//  Outline.cs
-//  QuickOutline
-//
-//  Created by Chris Nolet on 3/30/18.
-//  Copyright © 2018 Chris Nolet. All rights reserved.
-//
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor;
 using UnityEngine;
 
-[DisallowMultipleComponent]
-[ExecuteInEditMode]
-public class Outline : MonoBehaviour {
+namespace outline
+{
+  [DisallowMultipleComponent]
+  [ExecuteInEditMode]
+  public class Outline : MonoBehaviour {
 
-  public Color OutlineColor {
-    get { return outlineColor; }
-    set {
-      outlineColor = value;
-      _needsUpdate = true;
-    }
-  }
+    [SerializeField]
+    private Color outlineColor = Color.white;
 
-  public float OutlineWidth {
-    get { return outlineWidth; }
-    set {
-      outlineWidth = value;
-      _needsUpdate = true;
-    }
-  }
+    [SerializeField, Range(0f, 20f)]
+    private float outlineWidth = .5f;
 
-  [Serializable]
-  private class ListVector3 {
-    public List<Vector3> data;
-  }
+    private Material _outlineMaskMaterial;
+    private Material _outlineFillMaterial;
 
-  [SerializeField]
-  private Color outlineColor = Color.white;
+    private bool _needsUpdate = true;
+    void Awake() {
+      // Instantiate outline materials
+      _outlineMaskMaterial = Instantiate(Resources.Load<Material>(@"Shaders/outline/variant1/2DOutlineMask"));
+      _outlineFillMaterial = Instantiate(Resources.Load<Material>(@"Shaders/outline/variant1/2DOutline"));
 
-  [SerializeField, Range(0f, 20f)]
-  private float outlineWidth = .5f;
-
-  private Renderer[] _renderers;
-  private Material _outlineMaskMaterial;
-  private Material _outlineFillMaterial;
-
-  private bool _needsUpdate = true;
-  void Awake() {
-
-    // Cache renderers
-    _renderers = GetComponentsInChildren<Renderer>();
-
-    // Instantiate outline materials
-    _outlineMaskMaterial = Instantiate(Resources.Load<Material>(@"Shaders/outline/variant1/2DOutlineMask"));
-    _outlineFillMaterial = Instantiate(Resources.Load<Material>(@"Shaders/outline/variant1/2DOutline"));
-
-    _outlineMaskMaterial.name = "OutlineMask (Instance)";
-    _outlineFillMaterial.name = "OutlineFill (Instance)";
+      _outlineMaskMaterial.name = "OutlineMask (Instance)";
+      _outlineFillMaterial.name = "OutlineFill (Instance)";
     
-  }
+    }
 
-  void OnEnable() {
-    foreach (var renderer in _renderers) {
+    void OnEnable() {
+      foreach (var renderer in GetComponentsInChildren<Renderer>()) {
 
-      // Append outline shaders
-      var materials = renderer.sharedMaterials.ToList();
+        // Append outline shaders
+        var materials = renderer.sharedMaterials.ToList();
 
-      if(!IsHaveMaterials(materials, _outlineMaskMaterial.name))
-        materials.Add(_outlineMaskMaterial);
+        if(!IsHaveMaterials(materials, _outlineMaskMaterial.name))
+          materials.Add(_outlineMaskMaterial);
       
-      if(!IsHaveMaterials(materials, _outlineFillMaterial.name))
-        materials.Add(_outlineFillMaterial);
+        if(!IsHaveMaterials(materials, _outlineFillMaterial.name))
+          materials.Add(_outlineFillMaterial);
 
-      renderer.materials = materials.ToArray();
-    }
+        renderer.materials = materials.ToArray();
+      }
     
-    UpdateMaterialProperties();
-  }
-
-
-  void Update() {
-    UpdateMaterialProperties();
-
-    if (_needsUpdate) {
-      _needsUpdate = false;
-
+      UpdateMaterialProperties();
     }
-  }
 
-  void OnDisable() {
-    foreach (var renderer in _renderers) {
 
-      // Remove outline shaders
-      var materials = renderer.sharedMaterials.ToList();
+    void Update() {
+      UpdateMaterialProperties();
 
-      materials.Remove(_outlineMaskMaterial);
-      materials.Remove(_outlineFillMaterial);
+      if (_needsUpdate) {
+        _needsUpdate = false;
 
-      renderer.materials = materials.ToArray();
+      }
     }
-  }
+    void OnDisable() {
+      foreach (var renderer in GetComponentsInChildren<Renderer>()) {
 
-  void OnDestroy() {
+        // Remove outline shaders
+        var materials = renderer.sharedMaterials.ToList();
 
-    // Destroy material instances
-    DestroyImmediate(_outlineMaskMaterial);
-    DestroyImmediate(_outlineFillMaterial);
-  }
+        materials.Remove(_outlineMaskMaterial);
+        materials.Remove(_outlineFillMaterial);
 
-  void UpdateMaterialProperties() {
+        renderer.materials = materials.ToArray();
+      }
+    }
 
-    // Apply properties according to mode
-    _outlineFillMaterial.SetColor("_OutlineColor", outlineColor);
+    void OnDestroy() {
 
-    _outlineMaskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Greater);
-    _outlineFillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Always);
-    _outlineFillMaterial.SetFloat("_OutlineThickness", outlineWidth);
-  }
+      // Destroy material instances
+      DestroyImmediate(_outlineMaskMaterial);
+      DestroyImmediate(_outlineFillMaterial);
+    }
 
-  bool IsHaveMaterials( List<Material> materials, String tragetName)
-  {
-    for (int i = 0; i < materials.Count; i++)
+    void UpdateMaterialProperties() {
+
+      // Apply properties according to mode
+      _outlineFillMaterial.SetColor("_OutlineColor", outlineColor);
+
+      _outlineMaskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Greater);
+      _outlineFillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Always);
+      _outlineFillMaterial.SetFloat("_OutlineThickness", outlineWidth);
+    }
+
+    bool IsHaveMaterials( List<Material> materials, String tragetName)
     {
-      Material material = materials[i];
+      for (int i = 0; i < materials.Count; i++)
+      {
+        Material material = materials[i];
 
-      if (material.name == tragetName) return true;
+        if (material.name == tragetName) return true;
+      }
+
+      return false;
+
     }
-
-    return false;
-
   }
 }
