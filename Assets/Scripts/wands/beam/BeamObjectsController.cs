38eac9f74dab043ecb3cbbfba5a4cfce1166e9d0
Andrii Khudolii <khudolii.a.s@gmail.com>
2022-11-29 18:18:17 +0100

    feat: add hitmark where object was hitted; update force of pushing

--- Assets/Scripts/wands/beam/BeamObjectsController.cs
+++ Assets/Scripts/wands/beam/BeamObjectsController.cs
@@ -5,13 +5,15 @@ namespace wands.beam
     public class BeamObjectsController : MonoBehaviour
     {
         public GameObject raycastPointer;
+        public GameObject Bullet_Mark;
+
         private RaycastHit _rayOutput;
 
         public float targetObjectPointDistance;
 
         private const float MaxSpeed = 7f;
 
-        void FixedUpdate()
+        void Update()
         {
             var ray = new Ray(raycastPointer.transform.position, raycastPointer.transform.forward);
 
@@ -23,6 +25,18 @@ namespace wands.beam
                 
                 GameObject hittedObject = _rayOutput.transform.gameObject;
                 Rigidbody objectRigidbody = hittedObject.GetComponent<Rigidbody>();
+                
+                /// 
+
+                var hitMark = Instantiate(Bullet_Mark, _rayOutput.point, Quaternion.LookRotation(_rayOutput.normal));
+                hitMark.transform.Rotate(Vector3.up * 180);
+                hitMark.transform.Translate(Vector3.back * .01f);
+ 
+                hitMark.transform.SetParent(hittedObject.transform);
+                Destroy(hitMark, 1.5f);
+                
+                ///
+                
                 if (objectRigidbody == null) return;
 
                 if(objectRigidbody.velocity.magnitude > MaxSpeed)
@@ -30,7 +44,7 @@ namespace wands.beam
                     objectRigidbody.velocity = objectRigidbody.velocity.normalized * MaxSpeed;
                 }
                 
-                objectRigidbody.AddForce((_rayOutput.point - gameObject.transform.position).normalized * .2f,
+                objectRigidbody.AddForce((_rayOutput.point - gameObject.transform.position).normalized * .4f,
                     ForceMode.Impulse);
             }
         }
